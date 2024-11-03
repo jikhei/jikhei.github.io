b1a4f220d926f963155b19e965ef38d85501618a
Jikhei <CANJikhei1030@outlook.com>
2024-11-03 14:11:02 +0000

    Update

--- src/config.ts
+++ src/config.ts
@@ -30,11 +30,11 @@ export const siteConfig: SiteConfig = {
     depth: 2                // Maximum heading depth to show in the table, from 1 to 3
   },
   favicon: [    // Leave this array empty to use the default favicon
-    // {
-    //      src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-    // //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-    // //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-    // }
+    {
+         src: '/favicon/favicon-32.png',    // Path of the favicon, relative to the /public directory
+    //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+    //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+    }
   ]
 }
 
@@ -54,7 +54,7 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
   // avatar: false,
-  avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  avatar: 'assets/images/nagato-ava-2.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   name: 'Chen Yixi (陈翊晞)',
   bio: 'Can Jikhei (in Cantonese) | She | Ashley',
   links: [
