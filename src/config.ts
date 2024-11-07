e1919361782e4cad21b873df1f24a0b4eb7d3407
Jikhei <CANJikhei1030@outlook.com>
2024-11-07 05:11:14 +0000

    Update banner

--- src/config.ts
+++ src/config.ts
@@ -12,17 +12,17 @@ export const siteConfig: SiteConfig = {
   subtitle: 'Personal blog',
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
-    hue: 275,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    hue: 220,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
     fixed: true,     // Hide the theme color picker for visitors
   },
   banner: {
-    enable: false,
-    src: 'assets/images/demo-banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    enable: true,
+    src: 'assets/images/banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
     position: 'center',      // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
     credit: {
-      enable: false,         // Display the credit text of the banner image
-      text: '',              // Credit text to be displayed
-      url: ''                // (Optional) URL link to the original artwork or artist's page
+      enable: true,         // Display the credit text of the banner image
+      text: 'ON MY MIND',              // Credit text to be displayed
+      url: 'https://www.pixiv.net/artworks/122409257'                // (Optional) URL link to the original artwork or artist's page
     }
   },
   toc: {
